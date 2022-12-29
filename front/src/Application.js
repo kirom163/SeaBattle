7f996fabbf7202588d202f00bdce1ba525a74c76
Theophile-Guillard <PetrSpirt@yandex.ru>
2022-12-30 01:51:27 +0400

    production

--- front/src/Application.js
+++ front/src/Application.js
@@ -11,7 +11,7 @@ class Application {
 	constructor(scenes = {}) {
 		const mouse = new Mouse(document.body);
 		const player = new BattlefieldView(true);
-		const opponent = new BattlefieldView(true);//Здесь поменять на false, чтобы скрыть вражеские корабли, а так они видны
+		const opponent = new BattlefieldView(false);//Здесь поменять на false, чтобы скрыть вражеские корабли, а так они видны
 		const socket = io();
 
 		Object.assign(this, { mouse, player, opponent , socket});
